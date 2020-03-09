7a5302cbeab91738bce97548c7ca35f35c7ebd14
denis <denisstathatos@hotmail.com>
2020-03-09 11:36:22 -0400

    question 4

--- category-atelier.php
+++ category-atelier.php
@@ -7,13 +7,18 @@ get_header();
     <?php
 
 echo '<h2>' . category_description( get_category_by_slug( 'atelier' )) . '</h2>';
-    
-echo '<ol>';
+echo '<div class="gridAtelier" style="background-color:white; color:grey;">';
 while ( have_posts() ) :
     the_post();
-    echo '<li style="color:grey;"> <a style="color:grey">' . get_the_title() . '__________</a><a style="color:red;">' . get_post_field('post_name') . '</a><a>_________ ' . get_the_author_meta('display_name') . '</a>' . '</li>';
+
+    
+
+    echo '<div class="gridInterieur">';
+    echo '<div style=>' . get_the_title() . '</div><div>'. get_post_field('post_name') . '</div><div>'. get_the_author_meta('display_name') . '</div>';
+    echo '</div>';
+
 endwhile; // End of the loop.
- echo '</ol>';       
+ echo '</div>';       
 ?>
 </main>
 </div>
