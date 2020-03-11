7af29ecc08d7065a0aed0ebe8b85e591b8235218
denis <denisstathatos@hotmail.com>
2020-03-11 16:21:48 -0400

    excercice10

--- functions.php
+++ functions.php
@@ -1,9 +1,17 @@
 <?php
+
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
- 
+     $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version'));
+    wp_enqueue_script( 'main_js',get_stylesheet_directory_uri() . '/js/main.js', NULL, 1.0,true);
 }
+
+
 add_action( 'pre_get_posts', 'extraire_cours' );
 function extraire_cours( $query ) {
     if ($query->is_category('evenement'))
@@ -20,4 +28,13 @@ function extraire_cours( $query ) {
        $query->set( 'order', 'ASC' );
     }
 
+    if ($query->is_category('nouvelle'))
+    {
+       $query->set( 'posts_per_page', 10 );
+       $query->set( 'orderby', 'post_name' );
+       $query->set( 'order', 'ASC' );
+    }
+
  }
+
+ add_action( 'pre_get_posts', 'extraire_cours' );
