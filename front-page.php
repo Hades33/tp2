8723abdf6d4dfdbdd43ee1432339120498445a34
Stathatos <e1773130@cmaisonneuve.qc.ca>
2020-03-13 16:32:55 -0400

    ajout animation

--- front-page.php
+++ front-page.php
@@ -39,7 +39,7 @@ get_header();
 ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-
+        <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
         <?php
         
 
--- js/main.js
+++ js/main.js
@@ -81,6 +81,52 @@ function creationHTML(oDataJSON, oDiv){
     oDiv.innerHTML = monHtmlString; 
 }
 
+$(document).ready(function() {
+    setMatrix($(".site-title"), $(".site-title").html());
+    setMatrix($(".site-description"), $(".site-description").html());
+  });
+  
+  function setMatrix(elem, ctnt){
+    
+    var theLetters = "abcdefghijklmnopqrstuvwxyz#%&^+=-"; //You can customize what letters it will cycle through
+    var speed = 10; // ms per frame
+    var increment = 8; // frames per step. Must be >2
+  
+    var clen = ctnt.length;       
+    var si = 0;
+    var stri = 0;
+    var block = "";
+    var fixed = "";
+    //Call self x times, whole function wrapped in setTimeout
+    (function rustle (i) {          
+    setTimeout(function () {
+      if (--i){rustle(i);}
+      nextFrame(i);
+      si = si + 1;        
+    }, speed);
+    })(clen*increment+1); 
+    function nextFrame(pos){
+      for (var i=0; i<clen-stri; i++) {
+        //Random number
+        var num = Math.floor(theLetters.length * Math.random());
+        //Get random letter
+        var letter = theLetters.charAt(num);
+        block = block + letter;
+      }
+      if (si == (increment-1)){
+        stri++;
+      }
+      if (si == increment){
+      // Add a letter; 
+      // every speed*10 ms
+      fixed = fixed +  ctnt.charAt(stri - 1);
+      si = 0;
+      }
+      elem.html(fixed + block);
+      block = "";
+    }
+  }
+
 
 
 
